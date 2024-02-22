8302f721b3413e5cc021346f4f0f3535ab14c75e
문현우 <84183932+Munch310@users.noreply.github.com>
2024-02-22 21:45:30 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/0/181920. 카운트 업/카운트 업.cpp
+++ C++/프로그래머스/0/181920. 카운트 업/카운트 업.cpp
@@ -5,9 +5,8 @@ using namespace std;
 
 vector<int> solution(int start_num, int end_num) {
     vector<int> answer;
-    int length = end_num - start_num;
-    for(int i = 0; i <= length; i++){
-        answer.push_back(start_num + i);
+    for(int i = start_num; i <= end_num; i++){
+        answer.push_back(i);
     }
     return answer;
 }
