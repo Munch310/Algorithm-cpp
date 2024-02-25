71e3c58b0eede4c4786af17ea1bebdf9f59db739
문현우 <84183932+Munch310@users.noreply.github.com>
2024-02-26 00:19:27 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- C++/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ C++/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -6,17 +6,15 @@ using namespace std;
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     vector<string> answer(n);
-
     for(int i = 0; i < n; i++){
         bitset<16> bit1(arr1[i]);
         bitset<16> bit2(arr2[i]);
-
+        
         string mapLine = "";
         for(int j = n - 1; j >= 0; j--){
             mapLine += (bit1[j] | bit2[j]) ? '#' : ' ';
         }
         answer[i] = mapLine;
     }
-
     return answer;
 }
