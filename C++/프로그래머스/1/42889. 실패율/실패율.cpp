50ac764fa7f5dbb8e2ff8ef3aa887ffdbe937362
문현우 <84183932+Munch310@users.noreply.github.com>
2024-02-26 15:42:26 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- C++/프로그래머스/1/42889. 실패율/실패율.cpp
+++ C++/프로그래머스/1/42889. 실패율/실패율.cpp
@@ -8,14 +8,14 @@ vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
     vector<pair<double, int>> failRate;
     
-    for (int i = 1; i <= N; i++){
-        int notCleared = 0;
+    for(int i = 1; i <= N; i++){
         int reached = 0;
+        int notCleared = 0;
         
-        for (int j = 0; j < stages.size(); j++){
-            if (stages[j] > i){
+        for(int j = 0; j < stages.size(); j++){
+            if(stages[j] > i){
                 reached++;
-            } else if (stages[j] == i){
+            } else if(stages[j] == i){
                 notCleared++;
                 reached++;
             }
@@ -24,9 +24,10 @@ vector<int> solution(int N, vector<int> stages) {
         double rate = reached > 0 ? (double)notCleared / reached : 0;
         failRate.push_back({-rate, i});
     }
+    
     sort(failRate.begin(), failRate.end());
     
-    for (auto& fr : failRate){
+    for(auto& fr : failRate){
         answer.push_back(fr.second);
     }
     return answer;
