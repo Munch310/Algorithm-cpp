e44c28473b59fbf92eea936be15993c871804ba1
문현우 <84183932+Munch310@users.noreply.github.com>
2024-02-28 18:00:51 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- C++/프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.cpp
+++ C++/프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <cmath>
+#include <cctype>
 #include <vector>
 
 using namespace std;
@@ -8,11 +9,10 @@ int solution(string dartResult) {
     vector<int> scores;
     int score = 0;
     char bonus, option;
-
-    for (int i = 0; i < dartResult.length();) {
-        score = 0;
-        if (isdigit(dartResult[i])) {
-            if (dartResult[i + 1] == '0') {
+    
+    for(int i = 0; i < dartResult.length();){
+        if(isdigit(dartResult[i])){
+            if(dartResult[i] == '1' && dartResult[i + 1] == '0'){
                 score = 10;
                 i += 2;
             } else {
@@ -20,24 +20,24 @@ int solution(string dartResult) {
                 i++;
             }
         }
-
+        
         bonus = dartResult[i++];
-        if (bonus == 'D') score = pow(score, 2);
-        else if (bonus == 'T') score = pow(score, 3);
-
-        if (i < dartResult.length() && (dartResult[i] == '*' || dartResult[i] == '#')) {
+        if(bonus == 'D') score = pow(score, 2);
+        else if(bonus == 'T') score = pow(score, 3);
+        
+        if(i < dartResult.length() && (dartResult[i] == '*' || dartResult[i] == '#')){
             option = dartResult[i++];
-            if (option == '*') {
-                if (!scores.empty()) scores.back() *= 2;
+            if(option == '*'){
+                if(!scores.empty()) scores.back() *= 2;
                 score *= 2;
-            } else if (option == '#') {
+            } else if(option == '#'){
                 score *= -1;
             }
         }
-
+        
         scores.push_back(score);
     }
     int answer = 0;
-    for (int s : scores) answer += s;
+    for(int s : scores) answer += s;
     return answer;
 }
