0349a83fa6e0f24f4aa608a7893049cfaac551c4
문현우 <84183932+Munch310@users.noreply.github.com>
2024-03-01 23:59:28 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
+++ C++/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -4,10 +4,5 @@
 using namespace std;
 
 int solution(string my_string, string is_prefix) {
-    if(is_prefix.length() > my_string.length()) return 0;
-    if(my_string.substr(0, is_prefix.length()) == is_prefix){
-        return 1;
-    } else {
-        return 0;
-    }
+    return my_string.substr(0, is_prefix.size()) == is_prefix;
 }
