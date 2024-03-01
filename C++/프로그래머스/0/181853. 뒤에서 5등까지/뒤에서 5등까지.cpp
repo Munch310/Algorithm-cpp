6b7e08e1e4903d119bb1dc3185919e5c0580c00e
문현우 <84183932+Munch310@users.noreply.github.com>
2024-03-01 22:20:49 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- C++/프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
+++ C++/프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
@@ -6,6 +6,6 @@ using namespace std;
 
 vector<int> solution(vector<int> num_list) {
     sort(num_list.begin(), num_list.end());
-    vector<int> answer(num_list.begin(), num_list.begin() + 4 + 1);
+    vector<int> answer(num_list.begin(), num_list.begin() + 5);
     return answer;
 }
