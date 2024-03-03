6598afb9134a3898011d4104f1f2945305625d21
문현우 <84183932+Munch310@users.noreply.github.com>
2024-03-03 22:56:00 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    long long n = num;
+    while(n != 1 && answer < 500){
+        if(n % 2 == 0){
+            n /= 2;
+        } else{
+            n = n * 3 + 1;
+        }
+        answer++;
+    }
+    return (answer >= 500) ? -1 : answer;
+}
