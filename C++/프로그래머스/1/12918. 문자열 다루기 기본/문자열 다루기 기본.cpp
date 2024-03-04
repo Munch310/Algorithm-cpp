d4d3e69bf298f5bd35c9cbd40234727dd03ac973
문현우 <84183932+Munch310@users.noreply.github.com>
2024-03-04 16:47:22 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+bool solution(string s) {
+    if(s.length() != 4 && s.length() != 6) return false;
+    for(char c : s){
+        if (!isdigit(c)) return false;
+    }
+    return true;
+}
