1d3ac97aecb4da8b99adb6e27137209d4c98299c
문현우 <84183932+Munch310@users.noreply.github.com>
2024-03-04 20:59:00 +0900

    [level 1] Title: 최소직사각형, Time: 0.98 ms, Memory: 4.96 MB -BaekjoonHub

--- C++/프로그래머스/1/86491. 최소직사각형/README.md
+++ C++/프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 5.02 MB, 시간: 0.70 ms
+메모리: 4.96 MB, 시간: 0.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 04일 20:15:32
+2024년 03월 04일 20:58:58
 
 ### 문제 설명
 
--- C++/프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ C++/프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -1,19 +1,18 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 int solution(vector<vector<int>> sizes) {
     int maxW = 0;
     int maxH = 0;
-    for(auto& size : sizes){
+    for(auto size : sizes){
         int w = max(size[0], size[1]);
         int h = min(size[0], size[1]);
         
         maxW = max(maxW, w);
         maxH = max(maxH, h);
     }
-    int answer = maxW * maxH;
-    return answer;
+    return maxW * maxH;
 }
