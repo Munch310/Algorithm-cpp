05ffc94203ed321fa2306859d2767c4a414666df
문현우 <84183932+Munch310@users.noreply.github.com>
2024-03-07 14:36:15 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/1/12901. 2016년/2016년.cpp
+++ C++/프로그래머스/1/12901. 2016년/2016년.cpp
@@ -12,13 +12,13 @@ string solution(int a, int b) {
     mktime(&time);
     
     switch(time.tm_wday){
-        case 0 : answer += "SUN"; break;
-        case 1 : answer += "MON"; break;
-        case 2 : answer += "TUE"; break;
-        case 3 : answer += "WED"; break;
-        case 4 : answer += "THU"; break;
-        case 5 : answer += "FRI"; break;
-        case 6 : answer += "SAT"; break;
+        case 0 : answer = "SUN"; break;
+        case 1 : answer = "MON"; break;
+        case 2 : answer = "TUE"; break;
+        case 3 : answer = "WED"; break;
+        case 4 : answer = "THU"; break;
+        case 5 : answer = "FRI"; break;
+        case 6 : answer = "SAT"; break;
     }
     return answer;
 }
--- C++/프로그래머스/1/12901. 2016년/README.md
+++ C++/프로그래머스/1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.03 ms
+메모리: 4.21 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 07일 14:35:21
+2024년 03월 07일 14:36:13
 
 ### 문제 설명
 
