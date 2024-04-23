729bde3fbc9130e43d017b5e26e46eb205a90780
문현우 <84183932+Munch310@users.noreply.github.com>
2024-04-23 09:54:45 +0900

    [level 1] Title: K번째수, Time: 1.39 ms, Memory: 31.4 MB -BaekjoonHub

--- C#/프로그래머스/1/42748. K번째수/K번째수.cs
+++ C#/프로그래머스/1/42748. K번째수/K번째수.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 
 public class Solution {
     public int[] solution(int[] array, int[,] commands) {
@@ -8,10 +7,11 @@ public class Solution {
         for(int i = 0; i < rows; ++i)
         {
             int start = commands[i, 0] - 1;
-            int end = commands[i, 1];
+            int length = commands[i, 1] - commands[i, 0] + 1;
             int k = commands[i, 2];
             
-            int[] newArray = array.Skip(start).Take(end - start).ToArray();
+            int[] newArray = new int[length];
+            Array.Copy(array, start, newArray, 0, length);
             Array.Sort(newArray);
             answer[i] = newArray[k - 1];
         }
